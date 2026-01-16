a478c82d8f52643e9b943a5af3d0945a83adbdcb
Live_Sol <whqhrms0@gmail.com>
2026-01-16 10:28:56 +0900

    [26.01.16.] - 자동실행 원상복구

--- src/main/java/com/solar/CalculatorApplication.java
+++ src/main/java/com/solar/CalculatorApplication.java
@@ -1,9 +1,7 @@
 package com.solar;
 
-import jakarta.annotation.PreDestroy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class CalculatorApplication {
